d3dc97cea617988f8ecab275afb0d32573181038
Th3Sc0rp <exir2001@gmail.com>
2025-09-10 11:37:36 +0200

    start_screen

---
+++ scripts/Start_Screen/Start_Screen.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
