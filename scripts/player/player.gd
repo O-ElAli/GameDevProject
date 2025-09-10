6bd0ed16ba7ee50268450f26debd3a19e6ffe225
EragonBrisingr <geometrydasher26@gmail.com>
2025-09-11 01:41:00 +0200

    changed player movement to 4 directions only

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,12 +6,22 @@ var character_direction: Vector2
 
 func _physics_process(_delta: float) -> void:
 	
-	character_direction.x = Input.get_axis("move_left", "move_right")
-	character_direction.y = Input.get_axis("move_up", "move_down")
-	character_direction = character_direction.normalized()
+	#character_direction.x = Input.get_axis("move_left", "move_right")
+	#character_direction.y = Input.get_axis("move_up", "move_down")
+	#character_direction = character_direction.normalized()
 	
-	#Animation
+	character_direction = Vector2(0,0)
 	
+	if Input.is_action_pressed("move_down"):
+		character_direction.y = 1
+	elif Input.is_action_pressed("move_up"):
+		character_direction.y = -1
+	elif Input.is_action_pressed("move_right"):
+		character_direction.x = 1
+	elif Input.is_action_pressed("move_left"):
+		character_direction.x = -1
+	
+	#Animation	
 	if character_direction.x > 0: $sprite.animation = "Right"
 	elif character_direction.x < 0: $sprite.animation = "Left"
 	elif character_direction.y > 0: $sprite.animation = "Down"
