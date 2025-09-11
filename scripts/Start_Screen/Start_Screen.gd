28e35227a9220f2ffdf5b920bb6949077c2cb313
Th3Sc0rp <exir2001@gmail.com>
2025-09-11 13:02:48 +0200

    Fade + Prologue

--- scripts/Start_Screen/Start_Screen.gd
+++ scripts/Start_Screen/Start_Screen.gd
@@ -2,8 +2,18 @@ extends Node
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
-
+	$Fading_In/AnimationPlayer.play("Fade In")
+	await get_tree().create_timer(6.0).timeout
+	var tween := create_tween()
+	$Fading_In/Title_Prologue.self_modulate.a = 0.0
+	$Fading_In/Title_Prologue.visible = true
+	tween.tween_property($Fading_In/Title_Prologue, "self_modulate:a", 1.0, 2.0)
+	await tween.finished
+	await get_tree().create_timer(2.0).timeout
+	var tween_out := create_tween()
+	tween_out.tween_property($Fading_In/Title_Prologue, "self_modulate:a", 0.0, 2.0)
+	await tween_out.finished
+	get_tree().change_scene_to_file("res://Scenes/Prologue/Outside.tscn")
 
 func _on_continue_pressed() -> void:
 	pass # Replace with function body.
