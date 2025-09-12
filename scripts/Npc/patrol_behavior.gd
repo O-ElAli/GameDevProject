e349245eaddd5d7f6f969514d53ca837c07f604a
Th3Sc0rp <exir2001@gmail.com>
2025-09-12 19:46:15 +0200

    Bug

---
+++ scripts/Npc/patrol_behavior.gd
@@ -0,0 +1,77 @@
+@tool
+extends NPCBehavior
+
+const COLORS := [Color.RED, Color.YELLOW, Color.GREEN,Color.CYAN, Color.BLUE, Color.MAGENTA]
+
+@export var speed: float = 30.0
+
+var waypoints: Array[PatrolLocation] = []
+var index: int = 0
+var active_target: PatrolLocation
+
+func _ready() -> void:
+	_collect_waypoints()
+	if Engine.is_editor_hint():
+		child_entered_tree.connect(_collect_waypoints)
+		child_order_changed.connect(_collect_waypoints)
+		return
+	super._ready()
+	if waypoints.is_empty():
+		process_mode = Node.PROCESS_MODE_DISABLED
+		return
+	active_target = waypoints[0]
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	var threshold := 4.0
+	if npc.global_position.distance_to(active_target.target_position) <= threshold:
+		npc.global_position = active_target.target_position
+		npc.velocity = Vector2.ZERO
+		_start_next_segment()
+
+func _collect_waypoints(_n: Node = null) -> void:
+	waypoints.clear()
+	for child in get_children():
+		if child is PatrolLocation:
+			waypoints.append(child)
+
+	# Editor-only Darstellung
+	if Engine.is_editor_hint() and not waypoints.is_empty():
+		for i in range(waypoints.size()):
+			var wp := waypoints[i] as PatrolLocation
+			if not wp.tranform_changed.is_connected(_collect_waypoints):
+				wp.tranform_changed.connect(_collect_waypoints)
+			wp.update_label(str(i))                    # Zahl aktualisieren
+			wp.modulate = _color_for_index(i)          # Farbe zuweisen
+			var nxt := waypoints[(i + 1) % waypoints.size()]
+			wp.update_line(nxt.position)
+
+
+func _start_next_segment() -> void:
+	if not npc.allow_behavior or waypoints.size() < 2:
+		return
+
+	# Idle-Phase
+	npc.current_state = "idle"
+	npc.velocity = Vector2.ZERO
+	npc.play_animation()
+
+	var wait := active_target.wait_time
+	index = (index + 1) % waypoints.size()
+	active_target = waypoints[index]
+
+	await get_tree().create_timer(wait).timeout
+	if not npc.allow_behavior:
+		return
+
+	# Bewegungs-Phase
+	npc.current_state = "walk"
+	var dir := global_position.direction_to(active_target.target_position)
+	npc.facing = dir
+	npc.velocity = dir * speed
+	npc.face_towards(active_target.target_position)
+	npc.play_animation()
+
+func _color_for_index(i: int) -> Color:
+	return COLORS[i % COLORS.size()]
