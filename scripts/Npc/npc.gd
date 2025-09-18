ce3677582f7463ef94051dccd90e6cd8ae52c41d
EragonBrisingr <geometrydasher26@gmail.com>
2025-09-18 13:35:48 +0200

    merged with police + reorganized folders

--- scripts/Npc/npc.gd
+++ scripts/Npc/npc.gd
@@ -8,31 +8,13 @@ var facing: Vector2 = Vector2.DOWN
 var facing_label: String = "down"
 var allow_behavior: bool = true
 
-@export var npc_name: String = "NPC"
-var lines_dict = {
-	"Igor": [
-		'Hello, I\'m Igor!',
-		'Nice to meet you!'
-	]
-}
-
-var dialogue_lines: Array = []
-
 @export var data: NPCResource : set = _apply_data
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var animator: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	if lines_dict.has(npc_name):
-		print("if")
-		dialogue_lines = lines_dict[npc_name]
-	else:
-		print("else")
-		dialogue_lines = []
 	_initialize_from_resource()
-	face_towards(global_position + facing)
-	play_animation()
 	if Engine.is_editor_hint():
 		return
 	behavior_ready.emit()
@@ -41,18 +23,12 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func play_animation() -> void:
-	if animator:
-		animator.play(current_state + "_" + facing_label)
-	else:
-		if Engine.is_editor_hint():
-			return
-	
+	animator.play(current_state + "_" + facing_label)
 
 func face_towards(target: Vector2) -> void:
 	facing = global_position.direction_to(target)
 	_resolve_facing_label()
-	if sprite:
-		sprite.flip_h = facing_label == "side" and facing.x < 0
+	sprite.flip_h = facing_label == "side" and facing.x < 0
 
 func _resolve_facing_label() -> void:
 	var limit := 0.45
