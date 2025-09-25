15029e757a77c60ca0b60ec5d6443cfc54d4befe
EragonBrisingr <geometrydasher26@gmail.com>
2025-09-25 22:25:25 +0200

    minor changes to make player hud run

--- scripts/player_hud/player_hud.gd
+++ scripts/player_hud/player_hud.gd
@@ -12,37 +12,38 @@ class_name playerHUD extends Control
 
 
 func _ready():
-	if inventory:
-		inventory.inventory_changed.connect(_refresh_inventory)
-	_refresh_inventory()
+	#if inventory:
+		#inventory.inventory_changed.connect(_refresh_inventory)
+	#_refresh_inventory()
+	pass
 
-func _refresh_inventory() -> void:
-	# Alte Einträge löschen
-	for child in items_container.get_children():
-		child.queue_free()
-	if not inventory:
-		return
-
-	# Neue Items einfügen
-	for id in inventory.items.keys():
-		var count: int = inventory.items[id]
-		var item_data = _get_item_data_by_id(id)
-		if not item_data:
-			continue
-		var hbox = HBoxContainer.new()
-
-		# Icon
-		var icon_rect = TextureRect.new()
-		icon_rect.texture = item_data.icon
-		icon_rect.custom_minimum_size = Vector2(32, 32)
-		hbox.add_child(icon_rect)
-
-		# Label mit Font und Farbe
-		var lbl = Label.new()
-		lbl.text = "%dx %s" % [count, item_data.display_name]
-		lbl.label_settings = _make_label_settings()
-		hbox.add_child(lbl)
-		items_container.add_child(hbox)
+#func _refresh_inventory() -> void:
+	## Alte Einträge löschen
+	#for child in items_container.get_children():
+		#child.queue_free()
+	#if not inventory:
+		#return
+#
+	## Neue Items einfügen
+	#for id in inventory.items.keys():
+		#var count: int = inventory.items[id]
+		#var item_data = _get_item_data_by_id(id)
+		#if not item_data:
+			#continue
+		#var hbox = HBoxContainer.new()
+#
+		## Icon
+		#var icon_rect = TextureRect.new()
+		#icon_rect.texture = item_data.icon
+		#icon_rect.custom_minimum_size = Vector2(32, 32)
+		#hbox.add_child(icon_rect)
+#
+		## Label mit Font und Farbe
+		#var lbl = Label.new()
+		#lbl.text = "%dx %s" % [count, item_data.display_name]
+		#lbl.label_settings = _make_label_settings()
+		#hbox.add_child(lbl)
+		#items_container.add_child(hbox)
 
 func _on_exit_pressed() -> void:
 	hide()
